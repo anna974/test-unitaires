f72583646645166c4012e4a1130c73016d6256e7
anna974 <annapiaraly@gmail.com>
2019-03-05 14:34:07 +0400

    assert

--- testFiles/fileTest.js
+++ testFiles/fileTest.js
@@ -2,11 +2,22 @@ const assert = require("assert");
 var monScript = require('../lib/monScript');
 
 describe('compare_a_et_b', function() {
-    it('compare_a_etb', function(){
+    it('a est supérieur à b', function(){
         assert.equal(monScript.compare(8,6), true, "a est supérieur à b");
+    });
+    it('compare', function(){
         assert.equal(monScript.compare(6,8), false, "a n'est pas supérieur à b");
+    });
+    it('a est égal à b', function(){
         assert.equal(monScript.compare(8,8), false, "a est égal à b");
+    });
+    it('good', function(){
         assert.equal(monScript.compare(0,0), false, "a est égal à b");
     });
+    it('too good', function(){
+        assert.equal(monScript.compare(9,5), false, "good");
+    });
+
+
 })
 
